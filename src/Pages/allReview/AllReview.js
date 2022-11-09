12724e90cb8046751faad64192e67b7b0518b111
ibrahim <iibrahim01081998@gmail.com>
2022-11-09 21:24:01 +0600

    remove comments code

--- src/Pages/allReview/AllReview.js
+++ src/Pages/allReview/AllReview.js
@@ -1,35 +1,23 @@
 // import React, { useEffect, useState } from 'react';
-import './AllReviews.css'
+import "./AllReviews.css";
 
-const AllReview = ({review}) => {
-    console.log(review);
+const AllReview = ({ review }) => {
+  console.log(review);
 
-    const {name,photoURL,text}=review;
+  const { name, photoURL, text } = review;
 
-    // const [addreview,SetAddReviews]= useState({})
-
-    // useEffect(()=>{
-    //     fetch(`http://localhost:5000/reviews/${service}`)
-    //     .then(res => res.json())
-    //     .then(data => SetAddReviews(data))
-    // },[service])
-   
-    return (
-        
-        <div className='d-flex '>
-
-            <div>
-                <img src={photoURL} alt="" srcset="" className='rounded-circle' />
-            </div>
-            <div className='text-start ps-2 fs-6'>
-               <span> {name}</span>
-               <br />
-               <span> {text}</span>
-
-            </div>
-          
-        </div>
-    );
+  return (
+    <div className="d-flex ">
+      <div>
+        <img src={photoURL} alt="" srcset="" className="rounded-circle" />
+      </div>
+      <div className="text-start ps-2 fs-6">
+        <span> {name}</span>
+        <br />
+        <span> {text}</span>
+      </div>
+    </div>
+  );
 };
 
 export default AllReview;
