16bf425066b4e8a5cbca441da1ca5a046bb33088
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-11-10 22:02:52 +0600

    added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -36,7 +36,8 @@ const AddService = () => {
             fetch('https://b6a11-service-review-server-side-mohammadibrahim11.vercel.app/services',{
                 method:'POST',
                 headers: {
-                    'content-type' : 'application/json'
+                    'content-type' : 'application/json',
+                
                 },
                 body:JSON.stringify(addService)
 
