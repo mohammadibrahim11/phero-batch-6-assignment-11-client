96894e1941756c52dee8d8949ce7f87a71d66c62
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-11-10 09:06:12 +0600

    jwt added

--- src/Pages/Login/LogIn.js
+++ src/Pages/Login/LogIn.js
@@ -25,6 +25,22 @@ const LogIn = () => {
    signIn(email,password)
    .then(result=>{
       const user = result.user;
+      const currentUser ={
+        email:user.email
+      }
+      console.log(currentUser);
+
+      fetch('http://localhost:5000/jwt',{
+        method:'POST',
+        headers:{
+            'content-type' : 'application/json'
+        },
+        body:JSON.stringify(currentUser)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
     //   console.log(user);
       navigate(from, {replace:true});
       form.reset();
